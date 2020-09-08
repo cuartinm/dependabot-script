99e5cef73dcb74c34d1b6df1b149457df177d9ad
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-09-08 17:46:15 -0500

    using octokit

--- generic-update-script.rb
+++ generic-update-script.rb
@@ -8,20 +8,10 @@ require "dependabot/file_updaters"
 require "dependabot/pull_request_creator"
 require "dependabot/omnibus"
 require "gitlab"
-require "net/http"
-require "json"
+require 'octokit'
 
-token = ENV["GITHUB_ACCESS_TOKEN"]
-
-uri = URI.parse("https://api.github.com/users/cuartinm/repos")
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = (uri.scheme == 'https')
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-request = Net::HTTP::Post.new(uri.request_uri)
-request['Authorization'] = "token #{token}"
-response = http.request(request)
-json_response = JSON.parse(response.body)
-puts json_response
+client = Octokit::Client.new(:access_token => ENV["GITHUB_ACCESS_TOKEN"])
+client.user
 
 credentials = [
   {
