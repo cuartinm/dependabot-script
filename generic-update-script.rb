5ecefa42dbd76cedc9cd9cfaca6614af7e5693d0
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-09-08 17:33:27 -0500

    add token

--- generic-update-script.rb
+++ generic-update-script.rb
@@ -17,10 +17,9 @@ http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = (uri.scheme == 'https')
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 request = Net::HTTP::Post.new(uri.request_uri)
-request['Authorization'] = 'token #{token}'
+request['Authorization'] = "token #{token}"
 response = http.request(request)
 json_response = JSON.parse(response.body)
-puts token
 puts json_response
 
 credentials = [
