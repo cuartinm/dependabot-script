19ade22b4875f9cfbcc62b7b6da215470236618c
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-09-08 17:30:12 -0500

    add token

--- generic-update-script.rb
+++ generic-update-script.rb
@@ -10,12 +10,14 @@ require "dependabot/omnibus"
 require "gitlab"
 require 'net/http'
 
+token = ENV["GITHUB_ACCESS_TOKEN"]
+
 uri = URI.parse("https://api.github.com/users/cuartinm/repos")
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = (uri.scheme == 'https')
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 request = Net::HTTP::Post.new(uri.request_uri)
-request['Authorization'] = 'token ' + ENV["GITHUB_ACCESS_TOKEN"]
+request['Authorization'] = 'token #{token}'
 response = http.request(request)
 json_response = JSON.parse(response.body)
 puts json_response
