57723686344a099b24bf1a7ae2a4c71680b0e651
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-09-08 21:25:34 -0500

    remove script

--- update-script.rb
+++ update-script.rb
@@ -1,8 +1,5 @@
-# This script is designed to be copied into an interactive Ruby session, to
-# give you an idea of how the different classes in Dependabot Core fit together.
-#
-# It's used regularly by the Dependabot team to manually debug issues, so should
-# always be up-to-date.
+# This script is designed to loop through all dependencies in a GHE, GitLab or
+# Azure DevOps project, creating PRs where necessary.
 
 require "dependabot/file_fetchers"
 require "dependabot/file_parsers"
@@ -10,28 +7,30 @@ require "dependabot/update_checkers"
 require "dependabot/file_updaters"
 require "dependabot/pull_request_creator"
 require "dependabot/omnibus"
+require "gitlab"
+require 'octokit'
 
-# GitHub credentials with write permission to the repo you want to update
-# (so that you can create a new branch, commit and pull request).
-# If using a private registry it's also possible to add details of that here.
-credentials =
-  [{
+client = Octokit::Client.new(:access_token => ENV["GITHUB_ACCESS_TOKEN"])
+user = client.user 'cuartinm'
+puts user.name
+
+repositories = client.all_repositories
+puts repositories
+
+credentials = [
+  {
     "type" => "git_source",
     "host" => "github.com",
     "username" => "x-access-token",
-    "password" => "a-github-access-token"
-  }]
+    "password" => ENV["GITHUB_ACCESS_TOKEN"] # A GitHub access token with read access to public repos
+  }
+]
 
-# Full name of the GitHub repo you want to create pull requests for.
-repo_name = "github-account/github-repo"
+# Full name of the repo you want to create pull requests for.
+repo_name = ENV["PROJECT_PATH"] # namespace/project
 
 # Directory where the base dependency files are.
-directory = "/"
-
-# Name of the dependency you'd like to update. (Alternatively, you could easily
-# modify this script to loop through all the dependencies returned by
-# `parser.parse`.)
-dependency_name = "rails"
+directory = ENV["DIRECTORY_PATH"] || "/"
 
 # Name of the package manager you'd like to do the update for. Options are:
 # - bundler
@@ -49,20 +48,41 @@ dependency_name = "rails"
 # - submodules
 # - docker
 # - terraform
-package_manager = "npm_and_yarn"
+package_manager = ENV["PACKAGE_MANAGER"] || "bundler"
 
-source = Dependabot::Source.new(
-  provider: "github",
-  repo: repo_name,
-  directory: directory,
-  branch: nil
-)
+if ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"]
+  credentials << {
+    "type" => "git_source",
+    "host" => ENV["GITHUB_ENTERPRISE_HOSTNAME"], # E.g., "ghe.mydomain.com",
+    "username" => "x-access-token",
+    "password" => ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"] # A GHE access token with API permission
+  }
+
+  source = Dependabot::Source.new(
+    provider: "github",
+    hostname: ENV["GITHUB_ENTERPRISE_HOSTNAME"],
+    api_endpoint: "https://#{ENV['GITHUB_ENTERPRISE_HOSTNAME']}/api/v3/",
+    repo: repo_name,
+    directory: directory,
+    branch: "develop",
+  )
+else
+  source = Dependabot::Source.new(
+    provider: "github",
+    repo: repo_name,
+    directory: directory,
+    branch: "develop",
+  )
+end
 
 ##############################
 # Fetch the dependency files #
 ##############################
-fetcher = Dependabot::FileFetchers.for_package_manager(package_manager).
-          new(source: source, credentials: credentials)
+puts "Fetching #{package_manager} dependency files for #{repo_name}"
+fetcher = Dependabot::FileFetchers.for_package_manager(package_manager).new(
+  source: source,
+  credentials: credentials,
+)
 
 files = fetcher.files
 commit = fetcher.commit
@@ -70,6 +90,7 @@ commit = fetcher.commit
 ##############################
 # Parse the dependency files #
 ##############################
+puts "Parsing dependencies information"
 parser = Dependabot::FileParsers.for_package_manager(package_manager).new(
   dependency_files: files,
   source: source,
@@ -77,40 +98,67 @@ parser = Dependabot::FileParsers.for_package_manager(package_manager).new(
 )
 
 dependencies = parser.parse
-dep = dependencies.find { |d| d.name == dependency_name }
 
-#########################################
-# Get update details for the dependency #
-#########################################
-checker = Dependabot::UpdateCheckers.for_package_manager(package_manager).new(
-  dependency: dep,
-  dependency_files: files,
-  credentials: credentials,
-)
+dependencies.select(&:top_level?).each do |dep|
+  #########################################
+  # Get update details for the dependency #
+  #########################################
+  checker = Dependabot::UpdateCheckers.for_package_manager(package_manager).new(
+    dependency: dep,
+    dependency_files: files,
+    credentials: credentials,
+  )
 
-checker.up_to_date?
-checker.can_update?(requirements_to_unlock: :own)
-updated_deps = checker.updated_dependencies(requirements_to_unlock: :own)
+  next if checker.up_to_date?
 
-#####################################
-# Generate updated dependency files #
-#####################################
-updater = Dependabot::FileUpdaters.for_package_manager(package_manager).new(
-  dependencies: updated_deps,
-  dependency_files: files,
-  credentials: credentials,
-)
+  requirements_to_unlock =
+    if !checker.requirements_unlocked_or_can_be?
+      if checker.can_update?(requirements_to_unlock: :none) then :none
+      else :update_not_possible
+      end
+    elsif checker.can_update?(requirements_to_unlock: :own) then :own
+    elsif checker.can_update?(requirements_to_unlock: :all) then :all
+    else :update_not_possible
+    end
 
-updated_files = updater.updated_dependency_files
+  next if requirements_to_unlock == :update_not_possible
 
-########################################
-# Create a pull request for the update #
-########################################
-pr_creator = Dependabot::PullRequestCreator.new(
-  source: source,
-  base_commit: commit,
-  dependencies: updated_deps,
-  files: updated_files,
-  credentials: credentials,
-)
-pr_creator.create
+  updated_deps = checker.updated_dependencies(
+    requirements_to_unlock: requirements_to_unlock
+  )
+
+  #####################################
+  # Generate updated dependency files #
+  #####################################
+  print "  - Updating #{dep.name} (from #{dep.version})…"
+  updater = Dependabot::FileUpdaters.for_package_manager(package_manager).new(
+    dependencies: updated_deps,
+    dependency_files: files,
+    credentials: credentials,
+  )
+
+  updated_files = updater.updated_dependency_files
+
+  ########################################
+  # Create a pull request for the update #
+  ########################################
+  pr_creator = Dependabot::PullRequestCreator.new(
+    source: source,
+    base_commit: commit,
+    dependencies: updated_deps,
+    files: updated_files,
+    credentials: credentials,
+    assignees: [(ENV["PULL_REQUESTS_ASSIGNEE"] || ENV["GITLAB_ASSIGNEE_ID"])&.to_i],
+    label_language: true,
+    author_details: {
+      email: "dependabot@YOUR_DOMAIN",
+      name: "dependabot"
+    },
+  )
+  pull_request = pr_creator.create
+  puts " submitted"
+
+  next unless pull_request
+end
+
+puts "Done"
